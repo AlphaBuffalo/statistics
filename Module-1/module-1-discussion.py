86691b625994dc3c275fe99feda15e6d29d51fef
Blarg <bobby1821@hotmail.com>
2026-01-04 16:15:02 -0500

    Renamed main.py to module 1 discussion. pasted module 1 code for testing

---
+++ Module-1/module-1-discussion.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def main():
+    # input your data in the Python list below. 
+    # For example, if your temperature data is:  81, 79, 80, 85, 83, 85, 87, 84, 84, 88, 85, 87
+    # then the step below should be set as:   temperatures = [81, 79, 80, 85, 83, 85, 87, 84, 84, 88, 85, 87]
+    temperatures = [72, 76, 79, 78, 79, 79, 79, 80, 80, 78, 75, 75]
+
+    # prepare a dataframe for temperatures.
+    temperatures_df = pd.DataFrame(temperatures, columns=['temperature'])
+
+    # print temperatures dataframe
+    print(temperatures_df)
+
+
+
+
+
+    # Pandas dataframe has several methods that calculate descriptive statistics. 
+
+    # mean
+    mean = temperatures_df['temperature'].mean()
+    print("Mean=", round(mean,2))
+
+    # median
+    median = temperatures_df['temperature'].median()
+    print("Median=", round(median,2))
+
+    # variance
+    variance = temperatures_df['temperature'].var()
+    print("Variance=", round(variance,2))
+
+    # standard deviation
+    stdeviation = temperatures_df['temperature'].std()
+    print("Standard Deviation=", round(stdeviation,2))
+
+    # describe - a useful function that calculates several different descriptive statistics
+    statistics = temperatures_df['temperature'].describe()
+    print("")
+    print ("Describe method")
+    print (statistics)
+
+    # line chart
+    plt.plot(temperatures_df['temperature']) # plot
+
+    # setting a title for the plot, x-axis and y-axis
+    plt.title('Line plot of temperature data', fontsize=20) 
+    plt.xlabel('day')
+    plt.ylabel('temperature')
+
+    # show the plot
+    plt.show()
+
+
+if True:
+    main()
