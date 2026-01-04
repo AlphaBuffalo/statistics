2ddb7d3e8cfa5b3e949a57b11a3836642286ff8a
Blarg <bobby1821@hotmail.com>
2026-01-04 17:08:01 -0500

    Playing around with code some

--- Module-1/my-plot.py
+++ Module-1/my-plot.py
@@ -4,13 +4,44 @@ import matplotlib.pyplot as plt
 import argparse
 
 def main():
-    parser = argparse.ArgumentParser(description='My first stats program')
-    parser.add_argument('--data1')
-    parser.add_argument('--data2')
+    # parser = argparse.ArgumentParser(description='My first stats program')
+    # parser.add_argument('--data1')
+    # parser.add_argument('--data2')
 
-    args = parser.parse_args()
+    # args = parser.parse_args()
 
-    print(f'first arg: {args.data1} \nsecond arg: {args.data2}')
+    # print(f'first arg: {args.data1} \nsecond arg: {args.data2}')
+
+    crashes = sns.load_dataset("penguins")
+    df = crashes.loc[range(7)] 
+
+
+    # Intialize figure
+    f, ax = plt.subplots()
+            
+    # Plot total crashes
+    sns.set_color_codes("pastel")
+    sns.barplot(x="bill_length_mm", y="sex", data=df,
+                label="Total", color="b")
+
+    # # Plot crashes related to speeding
+    # sns.set_color_codes("muted")
+    # sns.barplot(x="speeding", y="abbrev", data=df,
+    #             label="Speeding-related", color="b")
+    # Set title
+    plt.title('Speeding-related automobile collisions', fontsize=20)
+
+    # Set legend
+    ax.legend(ncol=1, loc="lower right")
+    ax.set(xlim=(0, 28), ylabel="State",
+        xlabel="Automobile collisions (per billion miles)")
+
+    # Save the image
+    # plt.savefig("stacked.png")
+
+    # Show the image
+    plt.show()
+
 
 
 if True:
